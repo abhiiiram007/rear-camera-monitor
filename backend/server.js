c5f1f28d87cad84c8f56f89db82e60f7f3a83483
Delta Student <abhiramgvk@gmail.comm>
2026-06-16 12:56:21 +0530

    Production CORS

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
+   origin: "https://rear-camera-monitor.vercel.app",
     methods: ["GET", "POST"],
   },
 });
